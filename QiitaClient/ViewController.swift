343f7eb14326e6a5202f7f17a9f5c4cc9aa3c1eb
EtoE-Official <procs.fy@gmail.com>
2019-06-15 13:39:40 +0900

    Initial commit

---
+++ QiitaClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QiitaClient
+//
+//  Created by YoshimiHiromu on 2019/06/02.
+//  Copyright © 2019 hiring. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
