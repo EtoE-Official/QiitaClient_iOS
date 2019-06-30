4e601cbfb22ca4c597334fb46ec0b39252cf0ef2
EtoE-Official <procs.fy@gmail.com>
2019-06-30 12:04:30 +0900

    Add table view delegate

--- QiitaClient/ViewController.swift
+++ QiitaClient/ViewController.swift
@@ -6,6 +6,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
+        tableView.delegate = self
         
         let nib = UINib(nibName: "QiitaTableViewCell", bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: "QiitaTableViewCell")
@@ -24,5 +25,11 @@ extension ViewController: UITableViewDataSource {
         }
         cell.set(title: "タイトル\(indexPath.row)", author: "作成者")
         return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(indexPath.row)番目のセルをタップ")
     }
 }
